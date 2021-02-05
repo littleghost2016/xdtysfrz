554c08162aba257a6d09332c08941d431c975103
littleghost2016 <785340571@qq.com>
2021-02-05 11:46:47 +0800

    更新：调整程序入口布局

--- xdtysfrz.go
+++ xdtysfrz.go
@@ -1,17 +1,24 @@
 package xdtysfrz
 
+import (
+	myCrypto "xdtysfrz/crypto"
+)
+
+// GetEncryptedPassword 获取加密密码口令
 func GetEncryptedPassword(password, key, iv string) (encryptedPassword []byte) {
-	randomByteSlice1 := getARandomByteSliceOfTheSpecifiedLength(64)
-	constructedInput := append(randomByteSlice1, []byte(password)...)
-
-	encryptedPassword, _ = AESEncryptUsingCBCMode(constructedInput, []byte(key), []byte(iv))
-	return
-}
-
-func DecryptPassword(cryptedPassword []byte, key, iv string) (password []byte) {
-	passwordWithRandomByteSlice, _ := AESDecryptUsingCBCMode(cryptedPassword, []byte(key), []byte(iv))
-
-	password = passwordWithRandomByteSlice[64:]
+	encryptedPassword = myCrypto.EncryptPassword(password, key, iv)
 
 	return
 }
+
+// GetDecryptedPassword 获取解密后的密码口令
+func GetDecryptedPassword(cryptedPassword []byte, key, iv string) (password []byte) {
+	password = myCrypto.DecryptPassword(cryptedPassword, key, iv)
+
+	return
+}
+
+// GetCookie 获取模拟登录后的cookie
+// func GetCookie(username, password string) {
+
+// }
